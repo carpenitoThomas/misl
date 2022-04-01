3c1e968eaefc33feb06cacfde58b27e49b233672
carpenitoThomas <carpenito.t@gmail.com>
2022-04-01 08:57:00 -0400

    Updates the traceplot to remove points

--- R/trace_plot.R
+++ R/trace_plot.R
@@ -31,9 +31,9 @@ trace_plot <- function(imputations){
         dplyr::filter(!is.na(value)) %>%
         ggplot2::ggplot(ggplot2::aes(x = as.integer(iteration), y = value, group = m, color= m)) +
         ggplot2::geom_line() +
-        ggplot2::geom_point() +
         ggforce::facet_wrap_paginate(variable ~ statistic , scales = "free", ncol = 2, nrow = 3, page = page_num) +
-        ggplot2::theme_bw()
+        ggplot2::theme_bw() +
+        ggpot2::theme(legend.position="none")
     )
   }
 }
